6874e7d937c63c7bbc9a7e99f25ba8506b38fdef
thorlarsen <lar18033@byui.edu>
2021-06-04 21:28:46 -0500

    Week 7

--- wdd330-portfolio/Week5/hikes.js
+++ wdd330-portfolio/Week5/hikes.js
@@ -3,41 +3,40 @@
 //create an array of hikes
 const hikeList = [
     {
-      name: "Bechler Falls",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "3 miles",
-      difficulty: "Easy",
+      name: 'Bechler Falls',
+      imgSrc: 'falls.jpg',
+      imgAlt: 'Image of Bechler Falls',
+      distance: '3 miles',
+      difficulty: 'Easy',
       description:
-        "Beautiful short hike along the Bechler river to Bechler Falls",
+        'Beautiful short hike along the Bechler river to Bechler Falls',
       directions:
-        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+        'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
     },
     {
-      name: "Teton Canyon",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "3 miles",
-      difficulty: "Easy",
-      description: "Beautiful short (or long) hike through Teton Canyon.",
+      name: 'Teton Canyon',
+      imgSrc: 'falls.jpg',
+      imgAlt: 'Image of Bechler Falls',
+      distance: '3 miles',
+      difficulty: 'Easy',
+      description: 'Beautiful short (or long) hike through Teton Canyon.',
       directions:
-        "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Stateline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+        'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.'
     },
     {
-      name: "Denanda Falls",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "7 miles",
-      difficulty: "Moderate",
+      name: 'Denanda Falls',
+      imgSrc: 'falls.jpg',
+      imgAlt: 'Image of Bechler Falls',
+      distance: '7 miles',
+      difficulty: 'Moderate',
       description:
-        "Beautiful hike through Bechler meadows river to Denanda Falls",
+        'Beautiful hike through Bechler meadows river to Denanda Falls',
       directions:
-        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+        'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.'
     }
   ];
   
-  const imgBasePath = "//byui-cit.github.io/cit261/examples/";
-  //on load grab the array and insert it into the page on load
+  const imgBasePath = '//byui-cit.github.io/cit261/examples/';
   
   export default class Hikes {
     constructor(elementId) {
@@ -54,40 +53,97 @@ const hikeList = [
       return this.getAllHikes().find(hike => hike.name === hikeName);
     }
     //show a list of hikes in the parentElement
-    showHikeList() {}
+    showHikeList() {
+      this.parentElement.innerHTML = '';
+      // notice that we use our getter above to grab the list instead of getting it directly...this makes it easier on us if our data source changes...
+      renderHikeList(this.parentElement, this.getAllHikes());
+      this.addHikeListener();
+      // make sure the back button is hidden
+      this.backButton.classList.add('hidden');
+    }
     // show one hike with full details in the parentElement
-    showOneHike(hikeName) {}
+    showOneHike(hikeName) {
+      const hike = this.getHikeByName(hikeName);
+      this.parentElement.innerHTML = '';
+      this.parentElement.appendChild(renderOneHikeFull(hike));
+      // show the back button
+      this.backButton.classList.remove('hidden');
+    }
     // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
     addHikeListener() {
       // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
+      const childrenArray = Array.from(this.parentElement.children);
+      childrenArray.forEach(child => {
+        child.addEventListener('touchend', e => {
+          // why currentTarget instead of target?
+          this.showOneHike(e.currentTarget.dataset.name);
+        });
+      });
     }
     buildBackButton() {
-      const backButton = document.createElement("button");
-  
+      const backButton = document.createElement('button');
+      backButton.innerHTML = '&lt;- All Hikes';
+      backButton.addEventListener('touchend', () => {
+        this.showHikeList();
+      });
+      backButton.classList.add('hidden');
+      this.parentElement.before(backButton);
       return backButton;
-    }
+    } 
   }
+  // End of Hikes class
   // methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
-  function renderHikeList(parent, hikes) {}
+  function renderHikeList(parent, hikes) {
+    hikes.forEach(hike => {
+      parent.appendChild(renderOneHikeLight(hike));
+    });
+  }
   function renderOneHikeLight(hike) {
-    const item = document.createElement("li");
+    const item = document.createElement('li');
+    item.classList.add('light');
+    // setting this to make getting the details for a specific hike easier later.
+    item.setAttribute('data-name', hike.name);
     item.innerHTML = ` <h2>${hike.name}</h2>
-    <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-    <div>
-            <div>
-                <h3>Distance</h3>
-                <p>${hike.distance}</p>
-            </div>
-            <div>
-                <h3>Difficulty</h3>
-                <p>${hike.difficulty}</p>
-            </div>
-    </div>`;
+  <div class="container">
+  <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+  <div>
+          <div>
+              <h3>Distance</h3>
+              <p>${hike.distance}</p>
+          </div>
+          <div>
+              <h3>Difficulty</h3>
+              <p>${hike.difficulty}</p>
+          </div>
+  </div>
+  </div>`;
+  
     return item;
   }
   function renderOneHikeFull(hike) {
-    const item = document.createElement("li");
-  
+    const item = document.createElement('li');
+    item.innerHTML = ` 
+      
+          <img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}">
+          <h2>${hike.name}</h2>
+          <div>
+              <h3>Distance</h3>
+              <p>${hike.distance}</p>
+          </div>
+          <div>
+              <h3>Difficulty</h3>
+              <p>${hike.difficulty}</p>
+          </div>
+          <div>
+              <h3>Description</h3>
+              <p>${hike.description}</p>
+          </div>
+          <div>
+              <h3>How to get there</h3>
+              <p>${hike.directions}</p>
+          </div>
+      
+      `;
     return item;
   }
   
---
+++ wdd330-portfolio/todos-project/tasks.js
@@ -0,0 +1,14 @@
+let taskList = [
+  {
+    id: 1,
+    task: 'Do homework',
+    isDeleted: false,
+    isComplete: false
+  },
+  {
+    id: 2,
+    task: 'Wash car',
+    isDeleted: false,
+    isComplete: false
+  }
+]
