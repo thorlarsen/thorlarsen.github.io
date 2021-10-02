51e46b4de9ec03eeb2ef3a90b44c8896ed2818b2
thorlarsen <lar18033@byui.edu>
2021-10-01 19:15:20 -0500

    One last push

--- wdd330/js/main.js
+++ wdd330/js/main.js
@@ -1,8 +1,13 @@
-const links = [
+/* const links = [
   {
     label: "Week1",
     url: "week1/index.html"
   }
-]
+] */
 
+function writeFooter() {
+  const footer = document.getElementById('footer');
+  footer.innerHTML = "<div><p>&copy 2021 Thor Larsen | Web Frontend Dev II | BYU-Idaho</p></div>"
+} 
 
+window.addEventListener('load', writeFooter);
