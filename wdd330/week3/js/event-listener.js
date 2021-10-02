57aa8a46ca22fb38d0c546565bff6e0cdb4baae7
thorlarsen <lar18033@byui.edu>
2021-10-02 13:45:16 -0500

    Mostly finished events and dom page

--- wdd330/week3/js/event-listener.js
+++ wdd330/week3/js/event-listener.js
@@ -18,6 +18,34 @@ clickTestDiv.addEventListener('click', changeDivColor);
 function changeDivColor() {
     clickTestDiv.style.backgroundColor = "#ffffff"
 };
+clickTestDiv.addEventListener('dblclick', () => clickTestDiv.style.backgroundColor = "#ff0000" );
+
+const kdResp = document.getElementById('kd');
+const kpResp = document.getElementById('kp');
+const kuResp = document.getElementById('ku');
+addEventListener('keydown', ( event ) => kdResp.innerHTML = `<span id="kdout">You're holding ${event.key} </span>`);
+addEventListener('keypress', ( event ) => kpResp.innerHTML = `<span id="kpout">You pressed ${event.key} </span>`);
+addEventListener('keyup', ( event ) => {
+    kuResp.innerHTML = `<span id="kuout">You released ${event.key} </span>`;
+    kdResp.innerHTML = ' ';
+});
+
+const rmDiv = document.querySelector('#guidequotes');
+rmDiv.addEventListener('mouseover', changeGuideColor);
+function changeGuideColor() {
+    rmDiv.style.color = '#003152';
+    rmDiv.style.backgroundColor = '#aaaaaa';
+}
+rmDiv.addEventListener('mouseout', changeGuideBack);
+function changeGuideBack() {
+    rmDiv.style.color = '#aaaaaa';
+    rmDiv.style.backgroundColor = '#003152';
+}
+rmDiv.addEventListener('click', removeMouseOver);
+function removeMouseOver() {
+    changeGuideBack();
+    rmDiv.removeEventListener('mouseover', changeGuideColor);
+}
 
 
 const brokenLink = document.getElementById('broken');
