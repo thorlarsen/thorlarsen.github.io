b9b7b3cdfb8b34fd7135295d0d94690cc24377ea
thorlarsen <lar18033@byui.edu>
2021-10-07 19:09:43 -0500

    tic tac toe checks for winner
    Co-authored-by: naters123 <naters123@users.noreply.github.com>

---
+++ wdd330/week4/js/form-main.js
@@ -0,0 +1,3 @@
+const form = document.forms.search;
+const [input,button] = form.elements;
+const input = form.searchInput;
--- wdd330/week4/js/play-control.js
+++ wdd330/week4/js/play-control.js
@@ -14,8 +14,17 @@ function resetBoard(){
 function play(id) {   
   if (id.textContent != x && id.textContent != o) {
     id.innerHTML=player;
+    if (check4winner(player)) {
+      if (player==="X")
+      {
+        alert("Player 1 Wins");
+      }
+      else
+      {
+        alert("Player 2 Wins");
+      }
+    }
     changePlayer();
-    check4winner(id);
   }
 }
 
@@ -27,7 +36,67 @@ function changePlayer() {
   }
 }
 
-function check4winner(id) {
-
+function check4winner(player) {
+  if (document.getElementById("1").innerHTML === player && 
+    document.getElementById("2").innerHTML === player && 
+    document.getElementById("3").innerHTML === player)
+    {
+      return true;
+    }
+    else if (
+      document.getElementById("4").innerHTML === player && 
+    document.getElementById("5").innerHTML === player && 
+    document.getElementById("6").innerHTML === player
+    )
+    {
+      return true;
+    }
+    else if (
+      document.getElementById("7").innerHTML === player && 
+    document.getElementById("8").innerHTML === player && 
+    document.getElementById("9").innerHTML === player
+    )
+    {
+      return true;
+    }
+    if (document.getElementById("1").innerHTML === player && 
+    document.getElementById("4").innerHTML === player && 
+    document.getElementById("7").innerHTML === player)
+    {
+      return true;
+    }
+    else if (
+      document.getElementById("2").innerHTML === player && 
+    document.getElementById("5").innerHTML === player && 
+    document.getElementById("8").innerHTML === player
+    )
+    {
+      return true;
+    }
+    else if (
+      document.getElementById("3").innerHTML === player && 
+    document.getElementById("6").innerHTML === player && 
+    document.getElementById("9").innerHTML === player
+    )
+    {
+      return true;
+    }
+    else if (
+      document.getElementById("1").innerHTML === player && 
+    document.getElementById("5").innerHTML === player && 
+    document.getElementById("9").innerHTML === player
+    )
+    {
+      return true;
+    }
+    else if (
+      document.getElementById("3").innerHTML === player && 
+    document.getElementById("5").innerHTML === player && 
+    document.getElementById("7").innerHTML === player
+    )
+    {
+      return true;
+    }
+    return false;
 }
 
