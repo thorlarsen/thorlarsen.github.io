5c3f955d5ab96f8fb7dcd307a394101ac55c3fad
thorlarsen <lar18033@byui.edu>
2021-10-23 21:34:42 -0500

    Final turn in

--- wdd330/week6/js/controls.js
+++ wdd330/week6/js/controls.js
@@ -14,27 +14,38 @@ export default class Todos {
    
 } //end class definition
 
-
 function renderTodoList(listElement, todoList) {
     listElement.innerHTML = '';
     todoList.forEach(todo => renderOneTask(listElement, todo));
 }
 
 function renderOneTask(todoElement, todo) {
+  if (!todo.deleted) {
     let node = document.createElement('DIV');
     let textnode = document.createTextNode(todo.content);
     node.appendChild(textnode);
     node.classList.add("todoCard");
+    let doneButton = document.createElement('button');
+    doneButton.innerHTML = "Done";
+    if (todo.completed) { doneButton.classList.add("hiddenbtn","donebtn")}
+    else {doneButton.classList.add("donebtn")};
+    node.appendChild(doneButton);
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = "X";
+    if (todo.completed) { deleteButton.classList.add("hiddenbtn","delbtn")}
+    else {deleteButton.classList.add("delbtn")};
+    node.appendChild(deleteButton);
     todoElement.appendChild(node);
+  }
 }
 
 function genId() {
-    const id = Date.now;
-    return id;
+    const idNum = Date.now();
+    return idNum;
 }
 
-const actionButton = document.getElementById('add-button');
-actionButton.addEventListener('click', (e) => {
+const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', (e) => {
     e.preventDefault();
     addNewTask();
 });
@@ -44,6 +55,12 @@ function addNewTask() {
     todoList.push({content:newContent,id:newId,completed:false,deleted:false});
     const listElement = document.getElementById('todo-container');
     renderTodoList(listElement, todoList);
+    outputList(todoList);
+}
+ 
+function outputList(todoList) {
+    const todoJSONstring = JSON.stringify(todoList);
+    console.log(todoJSONout);
 }
 
 
