af16b44ae3fe186d8c1d1d07d0fb3961000c2860
thorlarsen <lar18033@byui.edu>
2021-10-23 19:14:31 -0500

    Todo Phase 2

--- wdd330/week6/js/controls.js
+++ wdd330/week6/js/controls.js
@@ -16,6 +16,7 @@ export default class Todos {
 
 
 function renderTodoList(listElement, todoList) {
+    listElement.innerHTML = '';
     todoList.forEach(todo => renderOneTask(listElement, todo));
 }
 
@@ -30,6 +31,19 @@ function renderOneTask(todoElement, todo) {
 function genId() {
     const id = Date.now;
     return id;
+}
+
+const actionButton = document.getElementById('add-button');
+actionButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addNewTask();
+});
+function addNewTask() {
+    const newContent = document.getElementById('new-content').value;
+    const newId = genId();
+    todoList.push({content:newContent,id:newId,completed:false,deleted:false});
+    const listElement = document.getElementById('todo-container');
+    renderTodoList(listElement, todoList);
 }
 
 
